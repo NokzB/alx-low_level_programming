3fc4f0191fab20312105ce72ddf826b90da14ba7
Noku <nnblb58@gmail.com>
2022-09-09 16:40:34 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - function to print lowercase letters
+*
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar ('\n');
+	return (0);
+}
+
