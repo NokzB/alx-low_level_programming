cb20a1774936462126c199bcf63ba1fe127381d9
Noku <nnblb58@gmail.com>
2022-09-09 18:29:32 -0700

    updates to task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,17 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 9; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
-		putchar((n % 10) + '0');
+		putchar(n);
+
 		if (n == 9)
-			continue;
-		putchar (',');
-		putchar(' ');
+		{
+
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
+	return (0);
 }
