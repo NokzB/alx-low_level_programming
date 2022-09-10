ee30802235e0eb5792bb1599cd3443c78c591e42
Noku <nnblb58@gmail.com>
2022-09-09 18:47:29 -0700

    updates to output spacing

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,15 +11,17 @@ int main(void)
 
 	for (n = '0'; n <= '9'; n++)
 	{
-		putchar(n);
+		putchar((n % 10) + '0');
 
-		if (n != 9)
-		{
+		if (n == 9)
+			continue;
 
-			putchar(',');
-			putchar(' ');
-		}
+		putchar(',');
+		putchar(' ');
+
 	}
+
 	putchar('\n');
+
 	return (0);
 }
