678a04cc16c56b8dd5e973edf905e0a4bf8e9e85
Noku <nnblb58@gmail.com>
2022-09-13 15:32:36 -0700

    added missing semicolon to main.h file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-#include <unistd.h>
-/**
-* _putchar - writes the characher c to stdout
-* @c: The character to print
-* 
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appro
-priately.
-*/
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+
+int _putchar(char c);
+
 #endif /* _MAIN_H_ */
