c07560b7e9bfd9d317ef0313465242a1d46b87aa
Noku <nnblb58@gmail.com>
2022-09-16 18:38:29 -0700

    task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_line - a function that prints a straight line
+*@n: input
+* Return: a straight line
+*/
+
+void print_line(int n)
+{
+	int a;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 1; a <= n; a++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
