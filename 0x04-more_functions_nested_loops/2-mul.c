19e9c7189e2d6953ea02466a771d5fbc840be5e1
Noku <nnblb58@gmail.com>
2022-09-16 17:54:31 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* mul - a functuib that mutiplies two numbers
+* @a: integer with a number
+* @b: integer with a number
+* Return: 1 on success
+*/
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
