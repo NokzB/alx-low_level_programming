710e969ce22ec43e369b6d1ceb55d842a49b5875
Noku <nnblb58@gmail.com>
2022-09-16 21:22:50 -0700

    update 2 to code

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,12 +18,20 @@ int main(void)
 		if ((i % 3 == 0 && i % 5 == 0))
 			printf("FizzBuzz ");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+		{
+			if (i == 100)
+			{
+				printf("Buzz ");
+				printf("\n");
+			}
+			else
+				printf("Buzz ");
+		}
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else
 			printf("%d ", i);
+	i++
 	}
-	printf("\n");
 	return (0);
 }
