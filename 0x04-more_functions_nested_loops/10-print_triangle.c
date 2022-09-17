46ae4a8f8f535eaed05f11b78595d2232fa13ea0
Noku <nnblb58@gmail.com>
2022-09-16 20:18:47 -0700

    removed trailing whitespace

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,15 +11,15 @@ void print_triangle(int size)
 	int b;
 
 	if (size <= 0)
-	{
+		_putchar(10);
 
-		for (a = size; a > 0; a--)
-		{
-			for (b = 1; b <= size; b++)
-				_putchar ('#');
-			_putchar('\n');
-		}
-	}
-	else
+	for (a = size; a > 0; a--)
+	{
+		for (b = 1; b <= size; b++)
+			if (b >= a)
+				_putchar(35);
+			else
+				_putchar(32);
 		_putchar('\n');
+	}
 }
