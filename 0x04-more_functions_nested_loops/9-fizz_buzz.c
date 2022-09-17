00fb1bcff3fdb6c0023c57d892c3d9656a3d9610
Noku <nnblb58@gmail.com>
2022-09-16 19:40:31 -0700

    added return function

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,18 +13,27 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 99; i++)
+	for (i = 1; i <= 100; i++)
 	{
 
-		if (i % 3 == 0 && i % 5 != 0)
+		if (i % 3 == 0)
+		{
 			printf("Fizz ");
-		else if (i % 5 == 0 && i % 3 != 0)
+		}
+		else if ((i % 5) == 0)
+		{
 			printf("Buzz ");
-		else if (i % 5 == 0 && i % 3 == 0)
+		}
+		else if (i % 15 == 0)
+		{
 			printf("FizzBuzz ");
+		}
 		else
-			printf("%i", i);
+		{
+			printf("%d ", i);
+		}
 	}
 	printf("Buzz");
 	printf("\n");
+	return (0);
 }
