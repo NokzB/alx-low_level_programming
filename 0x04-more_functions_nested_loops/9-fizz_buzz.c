ba2f0d6132d4d35737d2268eef6861231083df0b
Noku <nnblb58@gmail.com>
2022-09-16 21:31:17 -0700

    changed to for loop

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,28 +10,24 @@
 
 int main(void)
 {
-	int i = 1;
+	int i;
 
-	while (i <= 100)
+	for (i = 1; i <= 100; i++)
 	{
 
-		if ((i % 3 == 0 && i % 5 == 0))
-			printf("FizzBuzz ");
-		else if (i % 5 == 0)
+		if (i % 3 == 0)
 		{
-			if (i == 100)
-			{
-				printf("Buzz ");
-				printf("\n");
-			}
-			else
-				printf("Buzz ");
-		}
-		else if (i % 3 == 0)
 			printf("Fizz ");
-		else
-			printf("%d ", i);
-	i++
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 15 == 0)
+		{
+			printf("%d", i);
+		}
 	}
+	printf("\n");
 	return (0);
 }
