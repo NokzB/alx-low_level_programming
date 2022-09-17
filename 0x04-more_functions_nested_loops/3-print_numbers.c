17e7418f6208f8c53f9e7c030ab46a0281d2b0cf
Noku <nnblb58@gmail.com>
2022-09-16 18:05:56 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - prints numbers 0 - 9
+*
+* Return: 1 on success
+*/
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar (a + '0');
+	}
+	_putchar ('\n');
+}
