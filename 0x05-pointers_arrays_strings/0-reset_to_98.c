7f99c543e018e13c2b0e54ec26815bf34264854f
Noku <nnblb58@gmail.com>
2022-09-20 01:18:22 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* reset_to_98 - a function that updates a pointer value to 98
+* @n: parameter targeted
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains everything I've learned about pointers, arrays and strings and how I applied my knowledge
