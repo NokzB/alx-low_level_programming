7a30793d0d7d6f897a65b38a14762743fd33a1ac
Noku <nnblb58@gmail.com>
2022-09-21 01:44:40 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _strcat - concatenates two strings
+* @dest: the destination of the concatenated string
+* @src: the source of the string to be concatenated
+* Return: destination
+*/
+
+char *_strcat(char *dest, char *src)
+{
+        int length, i;
+        length = 0;
+
+        while (dest[length] != '\0')
+        {
+                ++length;
+        }
+        for (i = 0; src[i] != '\0'; ++i, ++length)
+        {
+                dest[length] = src[i];
+        }
+        dest[length] = '\0';
+        return (dest);
+}
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,27 @@
 #include "main.h"
-
 /**
-* _strcat - concatenates two strings
+* _strncat - concatenates two strings
 * @dest: the destination of the concatenated string
-* @src: the source of the string to be concatenated
+* @src: the source of the concatenated string
+* @n: an integer
 * Return: destination
 */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int length, i;
-	length = 0;
+
+	i = 0;
 
 	while (dest[length] != '\0')
 	{
 		++length;
 	}
-	for (i = 0; src[i] != '\0'; ++i, ++length)
+	while (src[i] != '\0' && i < n)
 	{
 		dest[length] = src[i];
+		++i;
+		++length;
 	}
 	dest[length] = '\0';
 	return (dest);
