d01dd6b82d4c752e7fd5ea299a93ee3fe41efdf6
Noku <nnblb58@gmail.com>
2022-09-21 03:55:16 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* leet - encodes a string into 1337
+* @str: string to be encoded
+* Return: encoded string
+*/
+
+char *leet(char *str)
+{
+	int i;
+	int j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (s1[j] == str[i])
+			{
+				str[i] = s2[j];
+			}
+		}
+	}
+	return (str);
+}
