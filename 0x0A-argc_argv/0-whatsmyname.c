276fa2f469e5520925a16de5fd45db07a47afec8
Noku <nnblb58@gmail.com>
2022-09-30 14:21:07 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - prints file name followed by a new line
+* @argc: number of arguments
+* @argv: array pointing to the arguments
+* Return: Always 0 (successful)
+*/
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This repository contains all the tasks given for the argc and argv project
