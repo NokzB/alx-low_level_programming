4fb366643cf7c76543a601abaeb50bff6f6c2a91
Noku <nnblb58@gmail.com>
2022-09-30 17:51:35 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* main - adds positive numbers
+* @argc: arguments
+* @argv: array of arguments in argc
+* Return: always 0 (successful)
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc < 1)
+	{
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
