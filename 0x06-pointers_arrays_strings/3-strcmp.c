d0d0fd5d1a726c3515a264e4a53940204189a473
Noku <nnblb58@gmail.com>
2022-10-01 07:25:49 -0700

    fixed declarations

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,13 +4,14 @@
 * _strcmp - compares two strings
 * @s1: first string to be compared to
 * @s2: second string to compare
-* Return: 0 if strings are the same, n0 if the are not
+*
+* Return: 0 if equal or j if is different
 */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int j;
+	int i = 0;
+	int j = 0;
 
 	while (s1[i] != '\0' && j == 0)
 	{
