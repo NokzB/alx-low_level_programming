5b74f7ea9f0378fb9f45d5f825147f7e4cc6a3eb
Noku <nnblb58@gmail.com>
2022-10-01 07:01:44 -0700

    betty coding style corrections

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,17 +9,18 @@
 
 char *_strcat(char *dest, char *src)
 {
-        int length, i;
-        length = 0;
+	int length, i;
+	length = 0;
 
-        while (dest[length] != '\0')
-        {
-                ++length;
-        }
-        for (i = 0; src[i] != '\0'; ++i, ++length)
-        {
-                dest[length] = src[i];
-        }
-        dest[length] = '\0';
-        return (dest);
+	while (dest[length] != '\0')
+	{
+		++length;
+	}
+
+	for (i = 0; src[i] != '\0'; ++i, ++length)
+	{
+		dest[length] = src[i];
+	}
+	dest[length] = '\0';
+	return (dest);
 }
