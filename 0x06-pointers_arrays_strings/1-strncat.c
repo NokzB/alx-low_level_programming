5896d5b3fa53acf10294543b697b342cdcdf1bac
Noku <nnblb58@gmail.com>
2022-10-01 07:08:36 -0700

    corrections

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,15 +13,15 @@ char *_strncat(char *dest, char *src, int n)
 
 	i = 0;
 
-	while (dest[length] != '\0')
+	while (dest[length] != 0)
 	{
-		++length;
+		length++;
 	}
-	while (src[i] != '\0' && i < n)
+	while (src[i] != 0 && i < n)
 	{
 		dest[length] = src[i];
-		++i;
-		++length;
+		i++;
+		length++;
 	}
 	dest[length] = '\0';
 	return (dest);
