02d0da34920c158492c48229992b1c6c4dbbcf3d
Noku <nnblb58@gmail.com>
2022-10-02 16:04:11 -0700

    correction on line 26 - 30

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -22,9 +22,9 @@ char *_strstr(char *haystack, char *needle)
 				if (needle[i + 1] == '\0')
 					return (haystack);
 				i++;
-			}
-		} while (haystack[i] == needle[i]);
-			haystack++;
+			} while (haystack[i] == needle[i]);
+		}
+		haystack++;
 	}
 	return ('\0');
 }
