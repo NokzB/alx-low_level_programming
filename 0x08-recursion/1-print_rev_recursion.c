946b39f441cefaa6ebbc71a580846d19a5626c15
Noku <nnblb58@gmail.com>
2022-10-03 17:43:45 -0700

    Changes to if statement

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 * _print_rev_recursion - prints a string in reverse
 * @s: string to be printed
@@ -7,7 +8,7 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
 		_print_rev_recursion(*s + 1);
 		_putchar(*s);
