757173e401219ef0ed912ccc5b9b0db6ec141980
Noku <nnblb58@gmail.com>
2022-10-03 19:10:01 -0700

    removed curly braces

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,18 +10,15 @@
 int _evaluate(int i, int n)
 {
 	if (n == 0 || n == 1)
-	{
 		return (n);
-	}
+
 	else if (i * i < n)
-	{
 		return (_evaluate(i + 1, n));
-	}
+
 	else if (i * i == n)
-	{
 		return (i);
-	}
 }
+
 /**
 * _sqrt_recursion - returns the natural square root of a number
 * @n: number input
@@ -33,11 +30,7 @@ int _sqrt_recursion(int n)
 	int i = 0;
 
 	if (i < 0)
-	{
 		return (-1);
-	}
-
 	else
 		return (_evaluate(i, n));
-
 }
