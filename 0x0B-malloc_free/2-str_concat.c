72eb4b3486ef1e318fd26f8f27ffd1250b52346b
Noku <nnblb58@gmail.com>
2022-10-04 15:28:50 -0700

    added for loop

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,14 +12,17 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s;
-	unsigned int i, j, k, l;
+	unsigned int i, j, k, limit;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+
+	for (j = 0; s2[j] != '\0'; j++)
 		;
 
 	s = malloc(sizeof(char) * (i + j + 1));
@@ -31,9 +34,9 @@ char *str_concat(char *s1, char *s2)
 
 		for (k = 0; k < i; k++)
 			s[k] = s1[k];
-		l = j;
+		limit = j;
 
-		for (j = 0; j <= i; k++, j++)
+		for (j = 0; j <= limit; k++, j++)
 			s[k] = s2[j];
 	return (s);
 }
