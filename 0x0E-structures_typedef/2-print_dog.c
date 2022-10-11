759d4e5fa6f66c677d5ae8607a7ffae2518ccfe2
Noku <nnblb58@gmail.com>
2022-10-11 03:21:41 -0700

    changed if statements

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (!d->name)
+		if (d->name)
 		{
 			printf("Name %s\n", d->name);
 		}
@@ -20,7 +20,7 @@ void print_dog(struct dog *d)
 		}
 		printf("Age: %f\n", d->age);
 
-		if (!d->owner)
+		if (d->owner)
 		{
 			printf("Owner: %s\n", d->owner);
 		}
