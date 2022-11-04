e384251fe19420650e36ca0357424c6bb79161a0
Noku <nnblb58@gmail.com>
2022-11-04 01:56:12 -0700

    fix 3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,17 +14,19 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	i = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (i < 0)
+		return (-1);
+
+
 	if (text_content != NULL)
 	{
 		for (length = 0; text_content[length];)
 			length++;
+		j = write(i, text_content, length);
+		if (j != length)
+			return (-1);
 	}
-
-	i = open(filename, O_CREAT || O_RDWR || O_TRUNC, 0600);
-	j = write(i, text_content, length);
-
-	if (i == -1 || j == -1)
-		return (-1);
 
 	close(i);
 
