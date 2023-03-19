19cbc55823e1a9cc631df9770d43669646c5ac33
Noku <nnblb58@gmail.com>
2023-03-19 06:39:04 -0700

    added parameter

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
 /**
- * _abs - Computes the absolute value of an integer.
- * @n: The integer to be computed.
- *
- * Return: The absolute value of the integer.
+ * _abs - returns absolute value of an integer
+ * @n: parameter
+ * Return: absolute value of an integer
  */
-int _abs(int)
-{
-	n = 0;
 
-	if (n >= 0)
-		return (n);
-	else
-		return (-n);
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
 }
