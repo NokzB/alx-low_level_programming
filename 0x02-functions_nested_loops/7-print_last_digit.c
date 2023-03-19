0034c847ccd34f532bdefd7f1325ff96966f6e5a
Noku <nnblb58@gmail.com>
2023-03-19 06:44:16 -0700

    initial commit got task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * print_last_digit - prints the last digit
+ * @n: it takes a number as an input
+ * Return: lastdigit
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	if (n < 0)
+		last = -1 * (n % 10);
+	else
+		last = n % 10;
+
+	_putchar(last + '0');
+	return (last);
+}
