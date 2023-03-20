928ef9a870d7c78e3eb279c39e63bd1b13795f23
Noku <nnblb58@gmail.com>
2023-03-19 23:52:15 -0700

    initial commit for task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and  return the results
+ * @i: first integer
+ * @j: second integer
+ * Return: sum of the integers
+ *
+ */
+int add(int i, int j)
+{
+	int sum = i + j;
+
+	return (sum);
+}
